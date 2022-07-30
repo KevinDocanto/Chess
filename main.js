110a4f779014024c8100a4a9325d81b9448ed0a6
Kevin Docanto <kevin-teus@hotmail.com>
2022-07-30 00:19:11 -0400

    Optimization...

--- main.js
+++ main.js
@@ -4,33 +4,19 @@ import { Pawn } from './pawn.js';
 const board = new Board();
 const white_pawns = new Array(8);
 const black_pawns = new Array(8);
-const pawn_width = 60;
-const pawn_height = 50;
 
 let pawn_white_x = 0;
 let pawn_white_y = 335;
 
-for (let i = 0; i < white_pawns.length; i++) {
-  white_pawns[i] = new Pawn(
-    './img/white_pawn.png',
-    pawn_white_x,
-    pawn_white_y,
-    pawn_width,
-    pawn_height
-  );
-  pawn_white_x += 55;
-}
-
 let pawn_black_x = 0;
 let pawn_black_y = 60;
 
-for (let i = 0; i < black_pawns.length; i++) {
-  white_pawns[i] = new Pawn(
-    './img/black_pawn.png',
-    pawn_black_x,
-    pawn_black_y,
-    pawn_width,
-    pawn_height
-  );
-  pawn_black_x += 55;
-}
+displayPieces = function (pieces, img, x, y) {
+  for (let i = 0; i < pieces.length; i++) {
+    white_pawns[i] = new Pawn(img, x, y);
+    x += 55;
+  }
+};
+
+displayPieces(white_pawns, './img/white_pawn.png', pawn_white_x, pawn_white_y);
+displayPieces(black_pawns, './img/black_pawn.png', pawn_black_x, pawn_black_y);
--- pawn.js
+++ pawn.js
@@ -1,12 +1,12 @@
 import { Piece } from './piece.js';
 
 export class Pawn extends Piece {
-  constructor(img, x, y, w, h) {
+  constructor(img, x, y) {
     super();
     this.x = x;
     this.y = y;
-    this.w = w;
-    this.h = h;
+    this.w = 60;
+    this.h = 50;
     this.displayPiece(img, this.x, this.y, this.w, this.h);
   }
 }
